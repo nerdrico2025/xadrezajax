59995242bda96bc288a88c505099bc4a9034ba3e
richwsb <countrwsb@gmail.com>
2026-04-29 11:02:23 -0300

    fix: corrige nome invalido no timezone

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
 LANGUAGE_CODE = "pt-br"
-TIME_ZONE = "America/Brazilia"
+TIME_ZONE = "America/Sao_Paulo"
 USE_I18N = True
 USE_TZ = True
 
